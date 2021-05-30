62dfcd4046e9f404dfb0f79765428114d072c89f
ohioshirt <g.ohioshirt+github@gmail.com>
2021-05-30 14:40:22 +0900

    Work In Progress with error

---
+++ app/src/main/java/com/example/android/hilt/data/LoggerInMemoryDataSource.kt
@@ -0,0 +1,24 @@
+package com.example.android.hilt.data
+
+import dagger.hilt.android.scopes.ActivityScoped
+import java.util.*
+import javax.inject.Inject
+
+@ActivityScoped
+class LoggerInMemoryDataSource @Inject constructor() : LoggerDataSource {
+
+    @JvmSuppressWildcards
+    private  val logs = LinkedList<Log>()
+
+    override fun addLog(msg: String) {
+        logs.add(Log(msg, System.currentTimeMillis()))
+    }
+
+    override fun getAllLogs(callbatk: (List<Log>) -> Unit) {
+        callbatk(logs)
+    }
+
+    override fun removeLogs() {
+        logs.clear()
+    }
+}
